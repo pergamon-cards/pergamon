8c95b3aee8f80cbc2666a8da6b0a6c7063b7106a
Matthew Waymost <matthew@waymost.net>
2025-11-15 19:32:30 -0800

    rustfmt

--- src/games/netrunner.rs
+++ src/games/netrunner.rs
@@ -1,53 +1,53 @@
 use serenity::builder::{CreateEmbed, CreateEmbedFooter, CreateMessage};
 
 pub fn create_embed(card: &serde_json::Value) -> CreateMessage {
-	let mut footer_string = String::default();
-	footer_string.push_str(faction_code(card["faction_code"].as_str().unwrap()));
-	footer_string.push_str(" \u{2022} ");
-	footer_string.push_str(card["pack_code"].as_str().unwrap());
-	footer_string.push_str(" #");
-	footer_string.push_str(&card["position"].to_string());
-	let footer = CreateEmbedFooter::new(footer_string);
-	
-	let mut url_string = String::default();
-	url_string.push_str("https://netrunnerdb.com/en/card/");
-	url_string.push_str(card["code"].as_str().unwrap());
-	
-	let mut img_url_string = String::default();
-	img_url_string.push_str("https://card-images.netrunnerdb.com/v2/large/");
-	img_url_string.push_str(card["code"].as_str().unwrap());
-	img_url_string.push_str(".jpg");
+    let mut footer_string = String::default();
+    footer_string.push_str(faction_code(card["faction_code"].as_str().unwrap()));
+    footer_string.push_str(" \u{2022} ");
+    footer_string.push_str(card["pack_code"].as_str().unwrap());
+    footer_string.push_str(" #");
+    footer_string.push_str(&card["position"].to_string());
+    let footer = CreateEmbedFooter::new(footer_string);
 
-	let embed = CreateEmbed::new()
-		.title(card["title"].as_str().unwrap())
-		.url(url_string)
-		.thumbnail(img_url_string)
-		.field(
-			format!(
-				"{}: {} - Cost: {} - Trash: {} - Influence: {}",
-				card["type_code"].as_str().unwrap(),
-				card["keywords"].as_str().unwrap(),
-				card["cost"],
-				card["trash_cost"],
-				card["faction_cost"]
-			),
-			card["text"].as_str().unwrap(),
-			false
-		)
-		.footer(footer);
+    let mut url_string = String::default();
+    url_string.push_str("https://netrunnerdb.com/en/card/");
+    url_string.push_str(card["code"].as_str().unwrap());
 
-	CreateMessage::new().embed(embed)
+    let mut img_url_string = String::default();
+    img_url_string.push_str("https://card-images.netrunnerdb.com/v2/large/");
+    img_url_string.push_str(card["code"].as_str().unwrap());
+    img_url_string.push_str(".jpg");
+
+    let embed = CreateEmbed::new()
+        .title(card["title"].as_str().unwrap())
+        .url(url_string)
+        .thumbnail(img_url_string)
+        .field(
+            format!(
+                "{}: {} - Cost: {} - Trash: {} - Influence: {}",
+                card["type_code"].as_str().unwrap(),
+                card["keywords"].as_str().unwrap(),
+                card["cost"],
+                card["trash_cost"],
+                card["faction_cost"]
+            ),
+            card["text"].as_str().unwrap(),
+            false,
+        )
+        .footer(footer);
+
+    CreateMessage::new().embed(embed)
 }
 
 fn faction_code(code: &str) -> &str {
-	match code {
-		"anarch" => "Anarch",
-		"criminal" => "Criminal",
-		"shaper" => "Shaper",
-		"nbn" => "NBN",
-		"jinteki" => "Jinteki",
-		"weyland-consortium" => "Weyland Consortium",
-		"hass-biodroid" => "Haas-Biodroid",
-		s => s,
-	}
+    match code {
+        "anarch" => "Anarch",
+        "criminal" => "Criminal",
+        "shaper" => "Shaper",
+        "nbn" => "NBN",
+        "jinteki" => "Jinteki",
+        "weyland-consortium" => "Weyland Consortium",
+        "hass-biodroid" => "Haas-Biodroid",
+        s => s,
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -15,68 +15,73 @@ use sqlx::Row;
 use crate::games::netrunner;
 
 struct State {
-	database: sqlx::SqlitePool,
+    database: sqlx::SqlitePool,
 }
 
 #[async_trait]
 impl EventHandler for State {
-	async fn message(&self, ctx: Context, msg: Message) {
-		// build here for debug only, shouldn't be here
-		let re = Regex::new(r"\[\[(?<title_query>.*)\]\]").unwrap();
-		let Some(q) = re.captures(&msg.content) else { return };
-		
-		let tq = &q["title_query"];
-		
-		let entry = sqlx::query("SELECT game, title, card FROM cards WHERE title = ? ORDER BY rowid LIMIT 1")
-			.bind(tq)
-			.fetch_optional(&self.database) // < Just one data will be sent to entry
-			.await
-			.unwrap();
-		
-		let builder = 
-			if let Some(row) = entry {
-				let card = row.get("card");
-				netrunner::create_embed(&card)
-			} else {
-				CreateMessage::new().content(&format!("{tq} not found"))
-			};
-			
-		let msg = msg.channel_id.send_message(&ctx.http, builder).await;
-		
-		if let Err(why) = msg {
-			println!("Error sending message: {why:?}");
-		}
-	}
+    async fn message(&self, ctx: Context, msg: Message) {
+        // build here for debug only, shouldn't be here
+        let re = Regex::new(r"\[\[(?<title_query>.*)\]\]").unwrap();
+        let Some(q) = re.captures(&msg.content) else {
+            return;
+        };
 
-	async fn ready(&self, _: Context, ready: Ready) {
-		println!("{} is connected!", ready.user.name);
-	}
+        let tq = &q["title_query"];
+
+        let entry = sqlx::query(
+            "SELECT game, title, card FROM cards WHERE title = ? ORDER BY rowid LIMIT 1",
+        )
+        .bind(tq)
+        .fetch_optional(&self.database) // < Just one data will be sent to entry
+        .await
+        .unwrap();
+
+        let builder = if let Some(row) = entry {
+            let card = row.get("card");
+            netrunner::create_embed(&card)
+        } else {
+            CreateMessage::new().content(&format!("{tq} not found"))
+        };
+
+        let msg = msg.channel_id.send_message(&ctx.http, builder).await;
+
+        if let Err(why) = msg {
+            println!("Error sending message: {why:?}");
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
 }
 
 #[tokio::main]
 async fn main() {
-	// Configure the client with your Discord bot token in the environment.
-	let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
-	
-	let database = sqlx::sqlite::SqlitePoolOptions::new()
-		.max_connections(5)
-		.connect_with(
-			sqlx::sqlite::SqliteConnectOptions::new()
-				.filename("data/cards.sqlite")
-				.create_if_missing(false),
-		)
-		.await
-		.expect("Couldn't connect to database");
-	
-	let state = State { database };
-	
-	let intents = GatewayIntents::GUILD_MESSAGES
-		| GatewayIntents::DIRECT_MESSAGES
-		| GatewayIntents::MESSAGE_CONTENT;
-	let mut client =
-		Client::builder(&token, intents).event_handler(state).await.expect("Err creating client");
+    // Configure the client with your Discord bot token in the environment.
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
 
-	if let Err(why) = client.start().await {
-		println!("Client error: {why:?}");
-	}
+    let database = sqlx::sqlite::SqlitePoolOptions::new()
+        .max_connections(5)
+        .connect_with(
+            sqlx::sqlite::SqliteConnectOptions::new()
+                .filename("data/cards.sqlite")
+                .create_if_missing(false),
+        )
+        .await
+        .expect("Couldn't connect to database");
+
+    let state = State { database };
+
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+    let mut client = Client::builder(&token, intents)
+        .event_handler(state)
+        .await
+        .expect("Err creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {why:?}");
+    }
 }
